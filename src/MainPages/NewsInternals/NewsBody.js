9098c962b0624e159ec80c317242fc0668040009
mattgf <mattgf@umich.edu>
2020-05-02 11:20:46 -0400

    news page done

--- src/MainPages/NewsInternals/NewsBody.js
+++ src/MainPages/NewsInternals/NewsBody.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ImageIntro from '../RepeatingComponents/introImage'
+import {Post} from '../RepeatingComponents/Post'
+
+import News from '../../Data/news.json'
+
 import { YellowBanner } from '../RepeatingComponents/YellowBanner';
 
 
@@ -12,6 +16,19 @@ function NewsBody() {
             <div>
         <ImageIntro imagePath = { `${process.env.PUBLIC_URL}/sam.png`}></ImageIntro>
         <YellowBanner>News</YellowBanner>
+
+        {News.map(post => {
+
+return(
+   <div>
+
+<Post optionalDate = {post.date} optionalTitle = {post.title} optionalIndex = {post.id}>
+{post.content}
+</Post>
+   </div>
+
+)
+})}
 
             </div>
 
