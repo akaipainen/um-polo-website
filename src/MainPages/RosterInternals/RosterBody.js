f73ba3f5a3f4926c6400af5eb88ae35d02d6ecec
mattgf <mattgf@umich.edu>
2020-05-02 10:42:53 -0400

    roster archive

--- src/MainPages/RosterInternals/RosterBody.js
+++ src/MainPages/RosterInternals/RosterBody.js
@@ -1,30 +1,69 @@
-import React from 'react'
+import React from 'react';
 import Player from '../RepeatingComponents/Player'
+import players2017 from '../../Data/players2017.json'
+import players2018 from '../../Data/players2018.json'
+import players2019 from '../../Data/players2019.json'
+
+import "./RosterBody.css"
+import { YellowBanner } from '../RepeatingComponents/YellowBanner'
+import { render } from '@testing-library/react';
 
 
-function RosterBody() {
-
-    return (
 
 
-        
+class RosterBody extends React.Component{
+constructor(){
+    super()
+    this.state = {
+        file: players2019,
+        year:2019
+    }
+    this.handleYear = this.handleYear.bind(this)
+}
+ handleYear(fileNew,yearNew){
+    this.setState({file: fileNew, year:yearNew}
+        )}
+    render(){
+        return(
             <div>
-         <Player 
-         id = {1}
-         isCaptian = {true}
-          hometown = "Fairfax, VA"
-           yearAndProgram = "Freshman studying computer science and industrial engineering"
-           headshotFile = "mFerrariHeadshot.png"
-           name ="Matthew Ferrari">
-         </Player>
-         
-            </div>
+            <YellowBanner>
+                Roster
+                    <div className="right">
+                    <div class="dropdown">
+                        <div class="dropbtn">{this.state.year} ↓</div>
+                        <div class="dropdown-content">
+                            <div className = {(2019 === this.state.year) +"button" } onClick={()=> this.handleYear(players2019,2019)}>2019</div>
+                            <div className = {(2018 === this.state.year) +"button" } onClick={()=> this.handleYear(players2018,2018)}>2018</div>
+                            <div className = {(2017 === this.state.year) +"button" } onClick={()=> this.handleYear(players2017,2017)}>2017</div>
+                        </div>
+                    </div>
+                </div>
+            </YellowBanner>
 
-     
+            {this.state.file.map(post => {
+
+                return (
+                    <div>
+
+                        <Player
+                            id={post.id}
+                            isCaptian={post.isCaptian}
+                            hometown={post.hometown}
+                            yearAndProgram={post.yearAndProgram}
+                            headshotFile={post.headshotFile}
+                            name={post.name}>
+                        </Player>
+
+                    </div>
+
+                )
+            })}
 
 
+        </div>
+        )
+    }
 
-    )
 }
 
 
