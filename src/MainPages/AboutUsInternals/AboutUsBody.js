da51c032b6f16caed00741552fa4447eaeab327d
Sid Murthy <sidmurthy221@gmail.com>
2020-05-02 00:16:41 -0400

    quick changes

--- src/MainPages/AboutUsInternals/AboutUsBody.js
+++ src/MainPages/AboutUsInternals/AboutUsBody.js
@@ -16,11 +16,12 @@ function AboutUsBody() {
             </YellowBanner>
 
             <Post>
-                <font size="+2"><p><b>National Champions</b><br />1996,1998,2003</p></font>
+                <font size="+2"><p><b>National Champions</b><br />1996, 1998, 2003</p></font>
                 <font size="+2"><p><b>Big Ten Champions</b><br />1989, 1991, 1992, 1996, 1997, 1998, 2003, 2004, 2011, 2012, 2015, 2016, 2017, 2018</p></font>
                 <font size="+2"><p><b>Big Ten Runner-Up</b><br />2000, 2001, 2002, 2005, 2006, 2008, 2009, 2010, 2014</p></font>
             </Post>
 
+            <YellowBanner>2020 Executive Board</YellowBanner>
 
         </div>
 
