bb242f9a8e800b371b02a2524332bc8a31c3048c
mattgf <mattgf@umich.edu>
2020-05-05 02:25:34 -0400

    slideshow

--- src/MainPages/HomeInternals/HomeBody.js
+++ src/MainPages/HomeInternals/HomeBody.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import Updates from '../../Data/updates.json'
 import "./HomeBody.css"
-import ImageIntro from '../RepeatingComponents/introImage'
 import SectionHeader from '../RepeatingComponents/SectionHeader/SectionHeader';
 import { Post } from '../RepeatingComponents/Post';
-
+import Slider from './show/components/Slider'
 
 
 
 function HomeBody() {
-
+   var arr = [
+        `${process.env.PUBLIC_URL}/mavShooting.png`,
+        `${process.env.PUBLIC_URL}/team.png`,
+        `${process.env.PUBLIC_URL}/banquet.png`,
+        `${process.env.PUBLIC_URL}/daBoys.png`
+    ]
+      
     return (        
             <div>
 
 
 
-        <ImageIntro imagePath = { `${process.env.PUBLIC_URL}/mavShooting.png`}></ImageIntro>
-        <a className = "streamingAlert"href = "https://youtube.com" rel="noopener noreferrer" target="_blank">Matches now streamed and archived on YouTube!</a>
+{    /*   <ImageIntro imagePath = { `${process.env.PUBLIC_URL}/mavShooting.png`}></ImageIntro>*/}
+
+<Slider slides={arr} autoPlay={5}/>
+<a className = "streamingAlert"href = "https://youtube.com" rel="noopener noreferrer" target="_blank">Matches now streamed and archived on YouTube!</a>
             <img src = { `${process.env.PUBLIC_URL}/champBanner.svg`} alt = "champ" className = "champ"></img>
          <SectionHeader title="Updates" />
                   
