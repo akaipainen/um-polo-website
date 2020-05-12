4bdd99ab9b73a9ec065262d54d8d976c2f13eb35
mattgf <mattgf@umich.edu>
2020-05-12 01:10:17 -0400

    final commit

--- src/MainPages/RepeatingComponents/EBoard.js
+++ src/MainPages/RepeatingComponents/EBoard.js
@@ -18,16 +18,16 @@ function EBoard(props) {
         
         
 
-       <div className = "nameHolder">
-        <div className = "name"> {name}</div>
+       <div className = "nameHolder1">
+        <div className = "name1"> {name}</div>
       
-        <div className = "hometown"> {hometown}</div>
-        <div className = "breakBottom"></div> 
+        <div className = "hometown1"> {hometown}</div>
+        <div className = "breakBottom1"></div> 
 
 
         </div>
       
-        {  headshotFile !=="undefined"&& headshotFile !== null && headshotFile !== "" && <img alt = "headshot" className = "headshot" src = { `${process.env.PUBLIC_URL}/${folder}/${headshotFile}`}></img>
+        {  headshotFile !=="undefined"&& headshotFile !== null && headshotFile !== "" && <img alt = "headshot" className = "headshot1" src = { `${process.env.PUBLIC_URL}/${folder}/${headshotFile}`}></img>
         }
       
       </div>
